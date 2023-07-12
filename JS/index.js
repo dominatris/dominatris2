98501f4ffc5b59c3c6405db41f594bb920604c25
dominatris <dominiqpariflores@gmail.com>
2023-07-12 16:43:03 -0300

    etet
    rwwer

--- JS/index.js
+++ JS/index.js
@@ -71,23 +71,39 @@ function solicitudAJAX(params) {
         if (objXMLHttpRequest.readyState === 4) {
           if (objXMLHttpRequest.status === 200) {
             let json = JSON.parse(objXMLHttpRequest.responseText);
-            let nombre = json.name;
-            let uriImg = json.sprites.other.home.front_default;
-            let html =
-              `<div class="card" style="width: 18rem;">
-    <img src="` +
-              uriImg +
-              `" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">` +
-              nombre +
-              `</h5>
-      <p class="card-text"></p>
-      <a href="https://www.wikidex.net/wiki/`+ nombre+
-      
-      `" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>`;
+          let nombre = json.name;
+          let nombreMayus = nombre.charAt(0).toUpperCase() + nombre.slice(1);
+          let uriImg = json.sprites.other.home.front_default;
+          let pokeID = json.id;
+          let pokeHP = json.stats[0].base_stat;
+          let pokeATK = json.stats[1].base_stat;
+          let pokeDEF = json.stats[2].base_stat;
+          let pokeSPD = json.stats[5].base_stat;
+          console.log(json)
+          let html =
+          `<div class="card" style="width: 18rem;">
+          <img src="` +
+                  uriImg +
+                  `" class="card-img-top" alt="...">
+          <div class="card-body"> <h3 class="card-title ftimpact">`+
+                  nombreMayus +
+                  `</h3>
+                  <span class="badge bg-secondary ftmonospace mb-3">` +
+                     pokeID +
+                     `</span>
+            <h5 class="card-text"><b>HP: </b>` +
+                  pokeHP +
+                  `&nbsp <b>ATK: </b>` +
+                  pokeATK +
+                  `<br> <b>DEF: </b>` +
+                  pokeDEF +
+                  `&nbsp <b>SPEED: </b>` +
+                  pokeSPD +
+            `</h5><a href="https://www.wikidex.net/wiki/` +
+                nombreMayus +
+            `" class="btn btn-primary">Ver más</a>
+          </div>
+        </div>`;
             tarjetas.innerHTML = html;
           } else {
             alert("Error Code: " + objXMLHttpRequest.status);
